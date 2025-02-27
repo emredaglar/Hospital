e1b49a7742b7d6ae5130d200d4503ec538440ba7
emredaglar <emre.daglar1@gmail.com>
2025-02-27 14:17:16 +0300

    Add project files.

---
+++ Services/Patient/Hospital.Patient/Entities/PatientUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital.Patient.Entities
+{
+    public class PatientUser
+    {
+        public int PatientUserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public string Gender { get; set; }
+    }
+}
